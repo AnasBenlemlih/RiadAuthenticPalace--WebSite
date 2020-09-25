767ca699c31f17e49c1954fcb3b75e3c7662fd11
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-09-25 19:39:48 +0100

    delete input email

--- contact_process.php
+++ contact_process.php
@@ -15,15 +15,11 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    //$subject = "You have a message from {$name} : riadfesauthenticpalace";
-
-    $logo = 'http://www.riadfesauthenticpalace.com/assets/img/logo/logo.png'; 
-    $link = 'www.riadfesauthenticpalace.com';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	
 	$body .= "</td></tr></thead><tbody><tr>";
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
