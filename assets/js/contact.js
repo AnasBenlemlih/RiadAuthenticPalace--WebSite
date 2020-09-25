26ff0c34c6df611556873ef069f944edab66af47
AnasBenlemlih <anas.benlemlih@gmail.com>
2020-09-25 19:04:20 +0100

    contact us

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -60,14 +60,7 @@ $(document).ready(function(){
                     data: $(form).serialize(),
                     url:"contact_process.php",
                     success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
+                        $(location).attr('href', 'http://www.riadfesauthenticpalace.com/successSendEmail.html');
                     },
                     error: function() {
                         $('#contactForm').fadeTo( "slow", 1, function() {
--- contact_process.php
+++ contact_process.php
@@ -1,11 +1,13 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "anas.benlemlih@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+	$cmessage = $_REQUEST['message'];
+	$cmobile = $_REQUEST['mobile'];
+	$pays = $_REQUEST['Pays'];
 
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
@@ -13,10 +15,10 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    //$subject = "You have a message from {$name} : riadfesauthenticpalace";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+    $logo = 'http://www.riadfesauthenticpalace.com/assets/img/logo/logo.png'; 
+    $link = 'www.riadfesauthenticpalace.com';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
@@ -25,10 +27,13 @@
 	$body .= "</td></tr></thead><tbody><tr>";
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr><tr>";
+	$body .= "<td style='border:none;'><strong>Num:</strong> {$cmobile}</td>";
+	$body .= "<td style='border:none;'><strong>Pays:</strong> {$pays}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'><strong>Message:</strong>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
